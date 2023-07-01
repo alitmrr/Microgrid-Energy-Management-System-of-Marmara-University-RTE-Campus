5b779fcab8abbff226bc6afcf1ecf9bb67126f9e
alitmrr <116978606+alitmrr@users.noreply.github.com>
2023-07-01 13:17:13 +0300

    Add files via upload

---
+++ veri_al.m
@@ -0,0 +1,17 @@
+
+m1=xlsread('m1.xlsx');
+
+m2=xlsread('m2.xlsx');
+m3=xlsread('m3.xlsx');
+m4=xlsread('m4.xlsx');
+m5=xlsread('m5.xlsx');
+ess=xlsread('generator.xlsx');
+generator=xlsread('ess.xlsx');
+pv=xlsread('pv.xlsx');
+a=xlsread('pv.xlsx');
+wind=xlsread('wind.xlsx');
+initial=[0 60];
+soc=0;
+in1=20;
+deneme=[0 60 ; 1 -50;2 60]
+
